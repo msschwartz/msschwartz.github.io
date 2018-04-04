eabfdff8606fdff1152e9b470f048c99470e03fe
Michael Schwartz <msschwartz@outlook.com>
2018-04-04 11:14:34 -0400

    Update fruitsnacks.sh

--- fruitsnacks.sh
+++ fruitsnacks.sh
@@ -1,3 +1,5 @@
+# How to use: 
+# curl -sLk "https://raw.githubusercontent.com/msschwartz/msschwartz.github.io/master/fruitsnacks.sh" | bash -
 
 # schedule a cron job to say "fruit snacks" once per minute
 crontab -l > tmpcron
