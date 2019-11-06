23c1142ed0f1e76ab7a2a4eb60a730b7058386fe
Michael Schwartz <msschwartz@outlook.com>
2019-11-06 14:38:18 -0500

    House

---
+++ assets/hayes/download.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+curl "https://api.claytonhomes.com/images/RETAIL/EXT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-20180126-1051153187720.png" > 1051153187720.png
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Master-Bedroom-20180202-0945446522604.jpg" > 0945446522604.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Study-Nook-20180202-0945447252718.jpg" > 0945447252718.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Dining-Area-20180202-0945448002784.jpg" > 0945448002784.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Dining-Area-20180202-0945448762865.jpg" > 0945448762865.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/EXT/Manufactured-EAST-BRANCH-6028-9054-SECT-81EHN28603DH-20180105-1242223047354.png" > 1242223047354.png
+
+curl "https://api.claytonhomes.com/images/RETAIL/EXT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-20180108-1008172008233.png" > 1008172008233.png
+
+curl "https://api.claytonhomes.com/images/RETAIL/FLP/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-20180115-1133047266401.jpg" > 1133047266401.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Kitchen-20180202-0945449402919.jpg" > 0945449402919.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Kitchen-20180202-0945450032955.jpg" > 0945450032955.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Kitchen-20180202-0945452143185.jpg" > 0945452143185.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Kitchen-20180202-0945454863430.jpg" > 0945454863430.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Bonus-Room-20180202-0945455533518.jpg" > 0945455533518.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Bonus-Room-20180202-0945456223567.jpg" > 0945456223567.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Guest-Bathroom-20180202-0945456893699.jpg" > 0945456893699.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Master-Bathroom-20180202-0945457473732.jpg" > 0945457473732.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Master-Bathroom-20180202-0945458073738.jpg" > 0945458073738.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Master-Bathroom-20180202-0945452753307.jpg" > 0945452753307.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Master-Bedroom-20180202-0945453443339.jpg" > 0945453443339.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Master-Bathroom-20180202-0945454223943.jpg" > 0945454223943.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Utility-Room-20180202-0945450763087.jpg" > 0945450763087.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/INT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-Living-Room-20180202-0945451523095.jpg" > 0945451523095.jpg
+
+curl "https://api.claytonhomes.com/images/RETAIL/EXT/Manufactured-HAYES-6028-9054-SECT-81SHN28603BH-20180126-1051107982061.png" > 1051107982061.png
+
+echo "done"
