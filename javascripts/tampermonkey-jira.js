0b8dd4ee13084d79e6bd4e9ab7ade93a386ace1e
Michael Schwartz <msschwartz@outlook.com>
2020-09-11 11:25:57 -0400

    Create tampermonkey-jira.js

---
+++ javascripts/tampermonkey-jira.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Jira Title Change for TrackingTime
+// @description  change the title in jira to be something useable in TrackingTime!
+// @namespace    https://msschwartz.github.io
+// @version      1.0
+// @author       Michael Schwartz
+// @match        https://*.atlassian.net/*
+// @match        https://jira.dm.nfl.com/*
+// @grant        none
+// @noframes     true
+// ==/UserScript==
+(function () {
+    "use strict";
+    var MutationObserver =
+        window.MutationObserver ||
+        window.WebKitMutationObserver ||
+        window.MozMutationObserver;
+
+    let lastTtc = false;
+
+    const body = document.querySelector("body");
+
+    const observer = new MutationObserver(mutations => {
+        const ttc = document.querySelector(".ttC");
+        if (ttc && ttc !== lastTtc) {
+            const ticketId = ttc.attributes['data-task_domain_id'].value;
+            // const ticketName = ttc.attributes['data-task_name'].value;
+            const title = document.querySelector('[data-test-id="issue.views.issue-base.foundation.summary.heading"]') || document.querySelector("#summary-val");
+            if (ticketId && title && title.innerHTML.indexOf(ticketId) === -1) {
+                lastTtc = ttc;
+                title.innerHTML = `${ticketId} | ${title.innerHTML}`;
+            }
+        }
+    });
+
+    observer.observe(body, { subtree: true, childList: true });
+})();
