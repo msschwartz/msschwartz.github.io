3a12328c591446e64fd9b5e340ba7164ce6b4e5e
Michael Schwartz <msschwartz@outlook.com>
2020-09-11 11:35:14 -0400

    Update tampermonkey-jira.js

--- javascripts/tampermonkey-jira.js
+++ javascripts/tampermonkey-jira.js
@@ -2,7 +2,7 @@
 // @name         Jira Title Change for TrackingTime
 // @description  Change the title in jira to be something useable in Tracking Time (Don't forget to add your custom domains in Tracking Time extension).
 // @namespace    https://msschwartz.github.io
-// @version      1.1
+// @version      1.2
 // @author       Michael Schwartz
 // @match        https://*.atlassian.net/*
 // @match        https://jira.dm.nfl.com/*
@@ -24,7 +24,6 @@
         const ttc = document.querySelector(".ttC");
         if (ttc && ttc !== lastTtc) {
             const ticketId = ttc.attributes['data-task_domain_id'].value;
-            // const ticketName = ttc.attributes['data-task_name'].value;
             const title = document.querySelector('[data-test-id="issue.views.issue-base.foundation.summary.heading"]') || document.querySelector("#summary-val");
             if (ticketId && title && title.innerHTML.indexOf(ticketId) === -1) {
                 lastTtc = ttc;
